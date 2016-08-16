71edbf8e6f50acd2cee054c499c555f5c49fac32
nickolanack <nickblackwell82@gmail.com>
2016-08-16 13:25:27 -0700

    update archive events to use generic event names, (was using deprecated events) and to also trigger monitor-able events so that flow can be monitored

--- customevent.emit.onDisplayMap.php
+++ customevent.emit.onDisplayMap.php
@@ -1,56 +1,72 @@
 <?php
 
-//onDisplayMap
+//onDisplayMap this handler is not necessary...
 
-function eventHandler(string $eventName, object $eventArguments){
-Core::LoadPlugin('Attributes');
+function eventHandler(string $eventName, object $eventArguments)
+{
+    Core::LoadPlugin('Attributes');
 
-$end = date('Y-m-d', time() - (30 * 24 * 3600));
-$table = 'markerAttributes';
-$tableMetadata = AttributesTable::GetMetadata($table);
-$field = 'sessionDate';
+    $end = date('Y-m-d', time() - (30 * 24 * 3600));
+    $table = 'markerAttributes';
+    $tableMetadata = AttributesTable::GetMetadata($table);
+    $field = 'sessionDate';
 /* @var $db MapsDatabase */
-$db = Core::LoadPlugin('Maps')->getDatabase();
-$markers = $db->table(MapsDatabase::$MAPITEM);
+    $db = Core::LoadPlugin('Maps')->getDatabase();
+    $markers = $db->table(MapsDatabase::$MAPITEM);
 
-$expiredEvents = json_decode(
-    '{
+    $expiredEvents = json_decode(
+        '{
                 "join":"join","table":"' . $table . '","set":"*","filters":[
                     {"join":"intersect","table":"' . $table . '","set":"*","filters":[
                         {"field":"' .
-         $field . '","comparator":"lessThan","value":"' . $end . '","format":"date"}
+        $field . '","comparator":"lessThan","value":"' . $end . '","format":"date"}
                     ]}
                 ]
             }');
 
-$queryExpired = 'Select m.id as id, m.lid as lid FROM ' . $markers . '  as m, ' .
-     AttributesFilter::JoinAttributeFilterObject($expiredEvents, 'm.id', 'm.type') . ' AND m.lid!=6';
-
-$db->iterate($queryExpired, 
-    function ($row) use($field, $tableMetadata) {
-        Core::Emit('custom.expire', array(
-            'mapitem' => $row->id
-        ));
-    });
-
-$revivedEvents = json_decode(
-    '{
+    $revivedEvents = json_decode(
+        '{
                 "join":"join","table":"' . $table . '","set":"*","filters":[
                     {"join":"intersect","table":"' . $table .
-         '","set":"*","filters":[
+        '","set":"*","filters":[
                         {"field":"' .
-         $field . '","comparator":"greatorThanOrEqualTo","value":"' . $end . '","format":"date"}
+        $field . '","comparator":"greatorThanOrEqualTo","value":"' . $end . '","format":"date"}
                     ]}
                 ]
             }');
 
-$queryRevived = 'Select m.id as id, m.lid as lid FROM ' . $markers . '  as m, ' .
-     AttributesFilter::JoinAttributeFilterObject($revivedEvents, 'm.id', 'm.type') . ' AND m.lid=6';
+    $queryExpired = 'Select m.id as id, m.lid as lid FROM ' . $markers . '  as m, ' .
+    AttributesFilter::JoinAttributeFilterObject($expiredEvents, 'm.id', 'm.type') . ' AND m.lid!=6';
 
-$db->iterate($queryRevived, 
-    function ($row) use($field, $tableMetadata) {
-        Core::Emit('custom.revive', array(
-            'mapitem' => $row->id
-        ));
-    });
+    $expiredItems = array();
+    $db->iterate($queryExpired,
+        function ($row) use ($field, $tableMetadata, &$expiredItems) {
+            Core::Emit('onArchiveItem', array(
+                'item' => $row->id,
+            ));
+            $expiredItems[] = $row->id;
+        });
+
+    $queryRevived = 'Select m.id as id, m.lid as lid FROM ' . $markers . '  as m, ' .
+    AttributesFilter::JoinAttributeFilterObject($revivedEvents, 'm.id', 'm.type') . ' AND m.lid=6';
+
+    $revivedItems = array();
+    $db->iterate($queryRevived,
+        function ($row) use ($field, $tableMetadata, &$revivedItems) {
+            Core::Emit('onUnarchiveItem', array(
+                'item' => $row->id,
+            ));
+            $revived[] = $row->id;
+
+        });
+
+    //For monitoring, if enabled, Otherwise no listeners should be bound to this event.
+
+    Core::Emit('onUpdateArchive', array(
+        'filterDate' => $end,
+        'expiredItems' => $expiredItems,
+        //'revivedItemsFilter' => $revivedEvents,
+        'revivedItems' => $revivedItems,
+    ));
+
 }
--- customevent.emit.php
+++ customevent.emit.php
@@ -3,41 +3,41 @@
 /**
  * @TODO deprecated events. should update to use onUpdateAttributeRecord, and onCreateAttributeRecord
  */
-
+// onUpdateAttributeRecord
+// onCreateAttributeRecord
+//
 //plugin.Attributes.onUpdateRecord.markerAttributes.*
 //plugin.Attributes.onCreateRecord.markerAttributes.*
 
-function customEventHandler(/*string*/ $eventName, /*object*/ $eventArguments){
+function customEventHandler( /*string*/$eventName, /*object*/ $eventArguments)
+{
 // decide whether to archive or unarchive
-if (! empty($eventArgs)) {
-    
-    Core::LoadPlugin('Attributes');
-    Core::LoadPlugin('Maps');
-    
-    $marker = MapController::LoadMapItem($eventArgs->mapitem);
-    $tableMetadata = AttributesTable::GetMetadata('markerAttributes');
-    $values = AttributesRecord::GetFields($marker->getId(), $marker->getType(), 'sessionDate', $tableMetadata);
-    
+    if (!empty($eventArgs)) {
 
-    $sessionDate=$values['sessionDate'];
+        Core::LoadPlugin('Attributes');
+        Core::LoadPlugin('Maps');
 
-    $date = strtotime($sessionDate);
-    $limit = time() - (30 * 24 * 3600);
+        $marker = MapController::LoadMapItem($eventArgs->item);
+        $tableMetadata = AttributesTable::GetMetadata('markerAttributes');
+        $values = AttributesRecord::GetFields($marker->getId(), $marker->getType(), 'sessionDate', $tableMetadata);
 
+        $sessionDate = $values['sessionDate'];
 
-    // Unarchive items without data, or within date range
+        $date = strtotime($sessionDate);
+        $limit = time() - (30 * 24 * 3600);
 
-    if (empty($sessionDate) || $date > $limit) {
-        file_put_contents(__DIR__ . DS . '.custom.log', 
-            'detect revive (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
-        Core::Emit('custom.revive', $eventArgs);
-    } else {
-        
-        file_put_contents(__DIR__ . DS . '.custom.log', 
-            'detect expire (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
-        
-        Core::Emit('custom.expire', $eventArgs);
+        // Unarchive items without data, or within date range
+
+        if (empty($sessionDate) || $date > $limit) {
+            file_put_contents(__DIR__ . DS . '.custom.log',
+                'detect revive (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
+            Core::Emit('onUnarchiveItem', $eventArgs);
+        } else {
+
+            file_put_contents(__DIR__ . DS . '.custom.log',
+                'detect expire (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
+
+            Core::Emit('onArchiveItem', $eventArgs);
+        }
     }
 }
-}
-
